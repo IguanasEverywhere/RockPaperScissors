6c9c98e060effb3a2b2f6e27b75255de4c2aacfc
Scott Schwab <schwabpiano@gmail.com>
2021-12-17 18:03:03 -0600

    Add UI and images

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // This function selects a random number betwee 0-2 and assigns it a corresponding string value which 
-// it then returns
+// it then returns as Rock, Paper, or Scissors value
 
 function computerPlay() {
     let computerSelection = Math.floor(Math.random() * 3);
@@ -23,80 +23,93 @@ function computerPlay() {
 }
 
 
-// This function gets the player Selection
 
-function getPlayerSelection() {
-    let playerEntry = prompt("Rock, Paper, or Scissors?");
-    let playerEntryLength = playerEntry.length;
-
-    playerSelection = playerEntry.substr(0, 1).toUpperCase() + playerEntry.substr(1, playerEntryLength).toLowerCase();
-
-    return playerSelection;
-}
-
-
-// This function runs the game. It calls another function function inside of it 5x that compares the 
-// computer choice to the player choice and increases the appropriate scores.
 
 function game() {
 
     let computerScore = 0;
     let playerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        playARound(computerPlay(), getPlayerSelection());
-    }
+    let playerSelection;
 
+    // button event listeners //////////////////////////////////////////////////
+
+    const rockBtn = document.getElementById("rockBtn");
+    rockBtn.addEventListener('click', () => {
+        playerSelection = "Rock";
+        playARound(computerPlay(), playerSelection);
+    });
+
+    const paperBtn = document.getElementById("paperBtn");
+    paperBtn.addEventListener('click', () => {
+        playerSelection = "Paper";
+        playARound(computerPlay(), playerSelection);
+    });
+
+    const scissorsBtn = document.getElementById("scissorsBtn");
+    scissorsBtn.addEventListener('click', () => {
+        playerSelection = "Scissors";
+        playARound(computerPlay(), playerSelection);
+    });
+
+    // function compares random computer selection to player selection, declares round winner, and increments appropriate score
 
     function playARound(computerSelection, playerSelection) {
 
         console.log("Computer's play is " + computerSelection);
         console.log("Player's selection is " + playerSelection);
 
+        let winnerDisplay = document.querySelector("#winnerDisplay");
+        let playerScoreDisplay = document.querySelector("#player-score");
+        let computerScoreDisplay = document.querySelector("#computer-score");
+        let choicesDisplay = document.querySelector("#display-selections");
+
         if (computerSelection === "Rock") {
             if (playerSelection === "Rock") {
-                console.log("It's a TIE!");
+                winnerDisplay.textContent="It's a TIE!";
             } else if (playerSelection === "Paper") {
-                console.log("You WIN! Paper beats Rock.");
+                winnerDisplay.textContent="You WIN! Paper beats Rock.";
                 playerScore++;
             } else if (playerSelection === "Scissors") {
-                console.log("You LOSE! Rock beats Scissors");
+                winnerDisplay.textContent="You LOSE! Rock beats Scissors";
                 computerScore++;
             }
         }
 
         if (computerSelection === "Paper") {
             if (playerSelection === "Rock") {
-                console.log("You LOSE! Paper beats Rock.");
+                winnerDisplay.textContent="You LOSE! Paper beats Rock.";
                 computerScore++;
             } else if (playerSelection === "Paper") {
-                console.log("It's a TIE!");
+                winnerDisplay.textContent="It's a TIE!";
             } else if (playerSelection === "Scissors") {
-                console.log("You WIN! Scissors beats Paper.");
+                winnerDisplay.textContent="You WIN! Scissors beats Paper.";
                 playerScore++;
             }
         }
 
         if (computerSelection === "Scissors") {
             if (playerSelection === "Rock") {
-                console.log("You WIN! Rock beats Scissors.");
+                winnerDisplay.textContent="You WIN! Rock beats Scissors.";
                 playerScore++;
             } else if (playerSelection === "Paper") {
-                console.log("You LOSE! Scissors beats Paper.");
+                winnerDisplay.textContent="You LOSE! Scissors beats Paper.";
                 computerScore++;
             } else if (playerSelection === "Scissors") {
-                console.log("It's a TIE!");
+                winnerDisplay.textContent="It's a TIE!";
             }
         }
-        console.log("Computer Score: " + computerScore + " Player Score: " + playerScore);
-    }
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+        choicesDisplay.textContent = "You chose " + playerSelection + " and Computer chose " + computerSelection;
 
-    if (computerScore > playerScore) {
-        console.log("Computer wins the 5 rounds!")
-    } else if (playerScore > computerScore) {
-        console.log("You win the 5 rounds!");
-    } else {
-        console.log("The 5 rounds was a tie!");
+
+        if (playerScore === 5 || computerScore === 5) {
+            winnerDisplay.textContent = "GAME OVER! Click a weapon to start again.";
+            playerScore = 0;
+            computerScore = 0;
+        }
+          
     }
 }
 
